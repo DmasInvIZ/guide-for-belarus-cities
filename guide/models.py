0ca113062f5426d6af227242a8513c006f0e577a
DmasInvIZ <inviz1806@gmail.com>
2022-09-24 22:34:41 +0300

    добавлено меню слева с выбором области страны

--- guide/models.py
+++ guide/models.py
@@ -29,9 +29,14 @@ class Towns(models.Model):
         verbose_name_plural = "Города"
 
 
-# class City(models.Model):
-#     author = models.CharField(max_length=20, default='User', null=True, blank=True)
-#     date = models.DateTimeField(auto_now=True)
-#     what_to_see = models.CharField(max_length=5000, blank=True)
-#     where_to_eat = models.CharField(max_length=5000, blank=True)
-#     where_to_sleep = models.CharField(max_length=5000, blank=True)
+class Districts(models.Model):
+    district = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='static/districts', null=True, blank=True)
+    text = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.district
+
+    class Meta:
+        verbose_name = "Область"
+        verbose_name_plural = "Области"
