190f306ec42cc45223af36e3a650a857e5db443a
DmasInvIZ <inviz1806@gmail.com>
2022-09-24 22:51:04 +0300

    добавлена модель по районам, сделаны миграции, работает

--- guide/admin.py
+++ guide/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import News, Towns
+from .models import News, Towns, Districts
 
 admin.site.register(News)
 admin.site.register(Towns)
+admin.site.register(Districts)
