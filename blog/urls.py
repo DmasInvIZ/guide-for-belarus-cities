9112723354e83734d962e4aaf158f76c46231a8e
DmasInvIZ <inviz1806@gmail.com>
2022-10-07 19:36:29 +0300

    при использовании login_required ошибка

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),  # изменить пост
     path('blog/new_post/', BlogCreateView.as_view(), name='new_post'),  # новый пост
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('blog/', BlogListView.as_view(), name='blog_home'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),  # детали поста
+    path('blog/', BlogListView.as_view(), name='blog_home'),  # страница с постами
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView  # новое изменение
+from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 
@@ -12,10 +13,17 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Post
-    template_name = 'blog_home.html'
+    template_name = 'post_detail.html'
 
 
+# @login_required
 class BlogCreateView(CreateView):  # новое изменение
     model = Post
     template_name = 'new_post.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView): # Новый класс
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
