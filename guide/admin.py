ca0d5a8267b4e37d430fbfa3c1e723f350f33cbe
DmasInvIZ <inviz1806@gmail.com>
2022-10-07 20:56:28 +0300

    мелкие фиксы раздела "блог проекта"

--- guide/admin.py
+++ guide/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import News, Towns, Districts, About
 
-admin.site.register(News)
 admin.site.register(Towns)
 admin.site.register(Districts)
 admin.site.register(About)
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'news', 'date')
+    search_fields = ['title']
+
+
+admin.site.register(News, NewsAdmin)
