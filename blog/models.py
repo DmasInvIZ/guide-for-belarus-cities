2ee8269bae0e90072eb6ff7a76052bb14ec29c62
DmasInvIZ <inviz1806@gmail.com>
2022-10-11 11:52:08 +0300

    ошибка при миграциях

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,10 @@ from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField('Заголовок', max_length=200)
-    # author = models.CharField('Автор', max_length=200, default='')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.CharField('Автор', max_length=200, default='')
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
     body = RichTextField('Пост', blank=True, null=True)  # вставка для красивого оформления поста
-    image = models.ImageField('Фотография', upload_to='static/blog', null=True, blank=True)
+    image = models.ImageField('Фотография', upload_to='static/blog', null=True, blank=True)  # надо удалить
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
