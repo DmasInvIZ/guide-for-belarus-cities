23486940d074be8952bffd88e14fc550d3992508
DmasInvIZ <inviz1806@gmail.com>
2022-10-11 11:13:08 +0300

    делаем миграции в приложении блога, меняем поле "автор" на залогиненого юзера

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from ckeditor.fields import RichTextField
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
 
 class Post(models.Model):
     title = models.CharField('Заголовок', max_length=200)
-    author = models.CharField('Автор', max_length=200, default='')
+    # author = models.CharField('Автор', max_length=200, default='')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     body = RichTextField('Пост', blank=True, null=True)  # вставка для красивого оформления поста
     image = models.ImageField('Фотография', upload_to='static/blog', null=True, blank=True)
     date = models.DateTimeField(auto_now=True)
