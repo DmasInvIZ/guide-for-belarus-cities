2802aca863c03c3a2a4c438d38d69ad682bfeb02
DmasInvIZ <inviz1806@gmail.com>
2022-11-07 16:12:51 +0300

    переделала маршруты, вроде работает

--- guide/urls.py
+++ guide/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path('news/<slug:slug>/', NewsDetailView.as_view(), name='news_detail'),  # детали новости с красивым URl
     path('about/', AboutView.as_view()),  # страница "О проекте"
     path('districts/', what_to_watch_view, name='districts'),  # вывод всех областей страны из бд
-    path('towns/<int:pk>', towns_view, name='towns'),  # вывод всех внесенных городов в данную область страны
+    path('districts/towns/<int:pk>', towns_view, name='towns'),  # вывод всех внесенных городов в данную область страны
     # path('towns/<int:pk>', TownsView.as_view()),
-    path('towns/town_watch/<int:pk>', town_watch_view),  # вывод конкретного города по запросу (что посмотреть)
-    path('towns/town_eat/<int:pk>', town_eat_view),  # вывод конкретного города по запросу (где поесть)
-    path('towns/town_sleep/<int:pk>', town_sleep_view),  # вывод конкретного города по запросу (где поспать)
+    path('districts/towns/town_watch/<int:pk>', town_watch_view),  # вывод конкретного города по запросу (что посмотреть)
+    path('districts/towns/town_eat/<int:pk>', town_eat_view),  # вывод конкретного города по запросу (где поесть)
+    path('districts/towns/town_sleep/<int:pk>', town_sleep_view),  # вывод конкретного города по запросу (где поспать)
     # path('town_watch/<int:pk>', TownWatchView.as_view(), name='town_watch'),  ####
     # path('town_eat/<int:pk>', TownEatView.as_view(), name='town_eat'),  #####
     # path('town_sleep/<int:pk>', TownSleepView.as_view(), name='town_sleep'),  ####
