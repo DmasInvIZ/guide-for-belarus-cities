a4a1b082704cd9e03d39437a87c0efa8c960a697
DmasInvIZ <inviz1806@gmail.com>
2022-11-27 21:56:30 +0300

    фиксы .sql файлов

--- удаление тестовых данных.sql
+++ удаление тестовых данных.sql
@@ -2,16 +2,9 @@
 Удаляет только тестовые данные, которые были внесены файлом для заполнения тестовых данных
 */
 
-DELETE FROM blog_post WHERE id > '1000';
-DELETE FROM guide_news WHERE id > '1000';
-DELETE FROM guide_about WHERE id > '1000';
-DELETE FROM guide_usertowns WHERE id > '1000';
-DELETE FROM guide_towns WHERE name = 'Полоцк';
-DELETE FROM guide_towns WHERE name = 'Новополоцк';
-DELETE FROM guide_towns WHERE name = 'Минск';
-DELETE FROM guide_towns WHERE name = 'Молодечно';
-DELETE FROM guide_towns WHERE name = 'Жодино';
-DELETE FROM guide_towns WHERE name = 'Вилейка';
-DELETE FROM guide_towns WHERE name = 'Витебск';
-DELETE FROM guide_districts WHERE name = 'Минская область';
-DELETE FROM guide_districts WHERE name = 'Витебская область';
+DELETE FROM blog_post WHERE id BETWEEN '1000' AND '1002';
+DELETE FROM guide_news WHERE id BETWEEN '1000' AND '1002';
+DELETE FROM guide_about WHERE id BETWEEN '1000' AND '1002';
+DELETE FROM guide_usertowns WHERE id BETWEEN '1000' AND '1003';
+DELETE FROM guide_towns WHERE name IN ('Полоцк', 'Новополоцк', 'Минск', 'Молодечно', 'Жодино', 'Вилейка', 'Витебск');
+DELETE FROM guide_districts WHERE name IN ('Минская область', 'Витебская область');
